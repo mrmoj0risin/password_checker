2f4811b763b19f8bc5263f9c3f468ae160d7fd3a
serj <serjst@icloud.com>
2021-03-30 13:06:56 +0300

    Initial commit

---
+++ pass_checker.py
@@ -0,0 +1,43 @@
+import requests
+import hashlib
+import sys
+
+
+def request_api_password(a):
+    url = 'https://api.pwnedpasswords.com/range/' + a
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error: {res.status_code} check what ur doing')
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            # print(count)
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    hashed_password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    five_chars, tail = hashed_password[:5], hashed_password[5:]
+    response = request_api_password(five_chars)
+    # print(response)
+    # print(five_chars,tail)
+    return get_password_leaks_count(response, tail)
+
+
+def main(args):
+  for password in args:
+    count = pwned_api_check(password)
+    if count:
+      print(f'{password} was found {count} times... you should probably change your password!')
+    else:
+      print(f'{password} was NOT found. Carry on!')
+  return '\n \n done!'
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
